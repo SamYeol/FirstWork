bc3bba469cc7a03436f8953a3c18ea48b2f92770
user56 <user56@gntp-56.gntp.local>
2018-04-27 14:45:09 +0900

    스캐너 추가

--- GitTest/src/HelloGit.java
+++ GitTest/src/HelloGit.java
@@ -1,7 +1,9 @@
-
+import java.util.Scanner;
 
 public class HelloGit {
 	public static void main(String args[]) {
 		System.out.println("hello");
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
 	}
 }
